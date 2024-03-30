3fcdb0e41043f683d9bae174d7073d91df672817
simonyang <simonyang777@qq.com>
2024-03-30 12:20:53 +0800

    format document: tab -> space

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -3,90 +3,90 @@ vim.cmd.source(HOME .. "/.vimrc")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
   {
-		"XXiaoA/ns-textobject.nvim",
-		config = function()
-			require("ns-textobject").setup({
-				disable_builtin_mapping = {
-					enabled = true,
-					-- list of char which shouldn't mapping by auto_mapping
-					chars = { "a" },
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"c",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"elixir",
-					"heex",
-					"javascript",
-					"html",
-					"vue",
-					"typescript",
-					"tsx",
-				},
-				sync_install = false,
-				highlight = { enable = false },
-				indent = { enable = true },
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-						},
-						include_surrounding_whitespace = true,
-					},
-				},
-			})
-		end,
-	},
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  {
+    "XXiaoA/ns-textobject.nvim",
+    config = function()
+      require("ns-textobject").setup({
+        disable_builtin_mapping = {
+          enabled = true,
+          -- list of char which shouldn't mapping by auto_mapping
+          chars = { "a" },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "elixir",
+          "heex",
+          "javascript",
+          "html",
+          "vue",
+          "typescript",
+          "tsx",
+        },
+        sync_install = false,
+        highlight = { enable = false },
+        indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+            include_surrounding_whitespace = true,
+          },
+        },
+      })
+    end,
+  },
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "/", mode = { "n", "x", "o" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>") }) end, desc = "Search Current" },
     },
-	},
+  },
 })
 
 vim.cmd([[
@@ -104,7 +104,6 @@ vim.cmd([[
   nmap k gk
   nnoremap zo <Cmd>lua require('vscode-neovim').call('editor.toggleFold')<CR>
 ]])
-
 
 -- Commands(:)
 -- x/wq       (save and close active tab)
@@ -184,7 +183,6 @@ vim.cmd([[
 -- <C-w>W   (focus previous window)
 -- <C-w>p   (focus previous window)
 
-
 -- Keybindings(insert mode)
 -- <C-r>{register}   (paste text from the {register})
 -- <C-h>    (same as backspace)
@@ -193,25 +191,23 @@ vim.cmd([[
 -- <C-j>    (same as enter)
 -- <C-o>    (enter normal mode then back to insert mode)
 
-
 -- Keybindings(visual mode)
 -- gv       (重选上次的高亮选区)
 -- o        (切换高亮选区的活动端)
 
-
 -- Plugins
--- nvim-surround
--- 修改: cs{old}{new}
--- cs'"
--- csq"
--- csqb
--- 删除: ds{old}
--- 增加: ys{range}{new}
--- 增加(visual mode): S{new}
--- 别名:
--- b   ()
--- B   {}
--- r   []
--- q   '', "",``
--- t   <tag></tag>
--- s  all
+  -- nvim-surround
+    -- 修改: cs{old}{new}
+        -- cs'"
+        -- csq"
+        -- csqb
+    -- 删除: ds{old}
+    -- 增加: ys{range}{new}
+    -- 增加(visual mode): S{new}
+    -- 别名: 
+      -- b   ()
+      -- B   {} 
+      -- r   []
+      -- q   '', "",``
+      -- t   <tag></tag>
+      -- s  all
